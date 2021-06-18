4cf0b3f01633aa8c3c432ca8bd3d70c2413acdcf
Bintang Pradana Erlangga Putra <bintangpradana02@student.uns.ac.id>
2021-06-18 16:25:08 +0700

    add delete functionality

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
+const moment = require('moment');
 
 router.get('/', async (req, res) => {
     try {
-        const posts = await Post.find().sort({date:-1});
+        const posts = await Post.find().sort({date: -1});
         res.send(posts);
     } catch(error) {
         res.send({ message: error });
@@ -21,6 +22,16 @@ router.post('/', async (req, res) => {
         res.json(savedPost);
     } catch(error) {
         res.json({ message: error });
+    }
+});
+
+router.delete('/', async (req, res) => {
+    try {
+        max_date = moment(Date.now()).subtract(24, 'hours');
+        const deletedPost = await Post.deleteMany({date: {$lte:  max_date}})
+        res.send(deletedPost)
+    } catch(error) {
+        res.send({ message: error })
     }
 });
 
